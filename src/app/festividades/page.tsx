a12e2c551ad454f7fdb4ad6f8decfd502ad4ea00
Luichix <luisreynaldo.pch@gmail.com>
2023-08-29 13:23:54 -0400

    fix(keys): add keys props in jsx
    add keys in jsx components departments and festivities

--- src/app/festividades/page.tsx
+++ src/app/festividades/page.tsx
@@ -1,151 +1,283 @@
-import classNames from "classnames";
-import allFestivities from '../../../public/data/festivities.json'
-import styles from './festividades.module.css'
+import classNames from 'classnames';
+import allFestivities from '../../../public/data/festivities.json';
+
+import styles from './festividades.module.css';
+
+const departamentos = [
+  'Chinandega',
+  'León',
+  'Managua',
+  'Masaya',
+  'Granada',
+  'Rivas',
+  'Carazo',
+  'Matagalpa',
+  'Jinotega',
+  'Nueva Segovia',
+  'Estelí',
+  'Madriz',
+  'Río San Juan',
+  'Chontales',
+  'Boaco',
+  'Atlántico Norte',
+  'Atlántico Sur',
+];
+
+const meses = [
+  { id: 1, nombre: 'Enero' },
+  { id: 2, nombre: 'Febrero' },
+  { id: 3, nombre: 'Marzo' },
+  { id: 4, nombre: 'Abril' },
+  { id: 5, nombre: 'Mayo' },
+  { id: 6, nombre: 'Junio' },
+  { id: 7, nombre: 'Julio' },
+  { id: 8, nombre: 'Agosto' },
+  { id: 9, nombre: 'Septiembre' },
+  { id: 10, nombre: 'Octubre' },
+  { id: 11, nombre: 'Noviembre' },
+  { id: 12, nombre: 'Diciembre' },
+];
 
 function getColor(region: string) {
-    switch (region) {
-        case 'Boaco':
-            return 'bg-red-400';
-        case 'Carazo':
-            return 'bg-green-400';
-        case 'Chinandega':
-            return 'bg-blue-400';
-        case 'Chontales':
-            return 'bg-yellow-400';
-        case 'Estelí':
-            return 'bg-indigo-400';
-        case 'Granada':
-            return 'bg-purple-400';
-        case 'Jinotega':
-            return 'bg-pink-400';
-        case 'León':
-            return 'bg-teal-500';
-        case 'Madriz':
-            return 'bg-orange-400';
-        case 'Managua':
-            return 'bg-blue-500';
-        case 'Masaya':
-            return 'bg-yellow-500';
-        case 'Matagalpa':
-            return 'bg-green-500';
-        case 'Nueva Segovia':
-            return 'bg-red-500';
-        case 'RACCN':
-            return 'bg-purple-500';
-        case 'RACCS':
-            return 'bg-pink-500';
-        case 'Río San Juan':
-            return 'bg-indigo-500';
-        case 'Rivas':
-            return 'bg-teal-600';
+  switch (region) {
+    case 'Boaco':
+      return 'bg-red-400';
+    case 'Carazo':
+      return 'bg-green-400';
+    case 'Chinandega':
+      return 'bg-blue-400';
+    case 'Chontales':
+      return 'bg-yellow-400';
+    case 'Estelí':
+      return 'bg-indigo-400';
+    case 'Granada':
+      return 'bg-purple-400';
+    case 'Jinotega':
+      return 'bg-pink-400';
+    case 'León':
+      return 'bg-teal-500';
+    case 'Madriz':
+      return 'bg-orange-400';
+    case 'Managua':
+      return 'bg-blue-500';
+    case 'Masaya':
+      return 'bg-yellow-500';
+    case 'Matagalpa':
+      return 'bg-green-500';
+    case 'Nueva Segovia':
+      return 'bg-red-500';
+    case 'RACCN':
+      return 'bg-purple-500';
+    case 'RACCS':
+      return 'bg-pink-500';
+    case 'Río San Juan':
+      return 'bg-indigo-500';
+    case 'Rivas':
+      return 'bg-teal-600';
 
-        // Agrega más casos para otros departamentos o regiones aquí
+    // Agrega más casos para otros departamentos o regiones aquí
 
-        default:
-            return 'bg-sky-600'; // Si no se encuentra la región, se establece un color predeterminado
-    }
+    default:
+      return 'bg-sky-600'; // Si no se encuentra la región, se establece un color predeterminado
+  }
 }
 
 // Función para formatear la fecha
 function formatDate(dateString: string) {
-    const date = new Date(dateString);
-    const monthNames = [
-        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-    ];
+  const date = new Date(dateString);
+  const monthNames = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
 
-    const month = monthNames[date.getUTCMonth()];
-    const day = date.getUTCDate();
+  const month = monthNames[date.getUTCMonth()];
+  const day = date.getUTCDate();
 
-    return `${month} ${day}`;
+  return `${month} ${day}`;
 }
 function formatFinalDate(dateStartString: string, dateEndString: string) {
-    const startDate = new Date(dateStartString);
-    const endDate = new Date(dateEndString);
-    const monthNames = [
-        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-    ];
+  const startDate = new Date(dateStartString);
+  const endDate = new Date(dateEndString);
+  const monthNames = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
 
-    const startMonth = monthNames[startDate.getUTCMonth()];
-    const endMonth = monthNames[endDate.getUTCMonth()];
-    const day = endDate.getUTCDate();
+  const startMonth = monthNames[startDate.getUTCMonth()];
+  const endMonth = monthNames[endDate.getUTCMonth()];
+  const day = endDate.getUTCDate();
 
-    if (startMonth === endMonth) {
-        return `${day}`;
-    }
-    return `${endMonth} ${day}`;
+  if (startMonth === endMonth) {
+    return `${day}`;
+  }
+  return `${endMonth} ${day}`;
 }
 
 interface DataFestive {
-    id: number
-    fechaInicio: string
-    fechaFin?: string
-    celebracion: string
-    regiones: string[]
-    descripcion: string
-    feriado: boolean
+  id: number;
+  fechaInicio: string;
+  fechaFin?: string;
+  celebracion: string;
+  regiones: string[];
+  descripcion: string;
+  feriado: boolean;
 }
-
-
-// Puedes utilizar este objeto "festividades" en tu aplicación React con el tipo de dato DataFestive.
-
-
-// Puedes utilizar este objeto "festividades" en tu aplicación React para llenar la tabla.
-
 
 export default function Festivities() {
-    return <div className="min-h-screen px-20 pb-20" >
+  const onPageChange = (page: number) => {
+    console.log(page);
+  };
 
-        <h2 className="text-2xl leading-relaxed tracking-wide py-10 text-center font-bold text-gray-700">Celebraciones en Nicaragua en 2023</h2>
-        <table className="min-w-full">
-            <thead>
-                <tr>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        N°
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Fecha
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Celebración
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Departamentos
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Feriado
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* <!-- Aquí se agregarán las filas de datos --> */}
-                {allFestivities.map(({ id, fechaInicio, fechaFin, celebracion, regiones, feriado }: DataFestive) => (
-                    <tr>
-                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            {id}
-                        </td>
-                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <span>{formatDate(fechaInicio)}</span> {fechaFin && <span>- {formatFinalDate(fechaInicio, fechaFin)}</span>}
-                        </td>
-                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            {celebracion}
-                        </td>
-                        <td className="px-6 py-4  border-b border-gray-200">
-                            <div className={styles.gridRegion}>
-                                {regiones.map((region) => (
-                                    <span className={classNames(`${getColor(region)}`, "max-w-min text-ellipsis overflow-hidden text-white rounded-xl whitespace-nowrap  px-3 text-center py-1 font-medium text-sm")}> {region}</span>
-                                ))}
-                            </div>
-                        </td>
-                        <td className="pl-10 pr-6 py-4 whitespace-no-wrap border-b  border-gray-200">
-                            {feriado ? <>🎉</> : <>-</>}
-                        </td>
-                    </tr>
-                )
-                )}
-            </tbody>
-        </table>
-
+  return (
+    <div className="min-h-screen px-20 pb-20">
+      <h2 className="text-2xl leading-relaxed tracking-wide py-10 text-center font-bold text-gray-700">
+        Celebraciones en Nicaragua en 2023
+      </h2>
+      {/* <div className="flex pb-8 gap-8">
+        <label
+          htmlFor="festividad"
+          className="flex text-sm text-gray-500 gap-2 items-center font-medium"
+        >
+          Festividad:
+          <input
+            id="festividad"
+            name="festividad"
+            type="text"
+            placeholder="Buscar"
+            className="py-2 px-3 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-300 bg-gray-100 placeholder:text-gray-600"
+          />
+        </label>
+        <label
+          htmlFor="departamento"
+          className="flex text-sm text-gray-500 gap-2 items-center font-medium"
+        >
+          Departamentos:
+          <select
+            id="departamento"
+            name="departamento"
+            // value={selectedDepartamento}
+            // onChange={handleSelectChange}
+            className="w-full p-2 border rounded-md shadow-sm focus:ring focus:ring-blue-300 outline-none bg-gray-100 text-gray-600"
+          >
+            <option value="">Todos</option>
+            {departamentos.map((departamento) => (
+              <option key={departamento} value={departamento}>
+                {departamento}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label
+          htmlFor="meses"
+          className="flex text-sm text-gray-500 gap-2 items-center font-medium"
+        >
+          Mes:
+          <select
+            id="meses"
+            name="meses"
+            // value={selectedDepartamento}
+            // onChange={handleSelectChange}
+            className="w-full p-2 border rounded-md shadow-sm focus:ring focus:ring-blue-300 outline-none bg-gray-100 text-gray-600"
+          >
+            <option value="">Todos</option>
+            {meses.map((mes) => (
+              <option key={mes.id} value={mes.nombre}>
+                {mes.nombre}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div> */}
+      <table className="min-w-full">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              N°
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Fecha
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Celebración
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Departamentos
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Feriado
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <!-- Aquí se agregarán las filas de datos --> */}
+          {allFestivities.map(
+            ({
+              id,
+              fechaInicio,
+              fechaFin,
+              celebracion,
+              regiones,
+              feriado,
+            }: DataFestive) => (
+              <tr key={id}>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  {id}
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  <span>{formatDate(fechaInicio)}</span>{' '}
+                  {fechaFin && (
+                    <span>- {formatFinalDate(fechaInicio, fechaFin)}</span>
+                  )}
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  {celebracion}
+                </td>
+                <td className="px-6 py-4  border-b border-gray-200">
+                  <div className={styles.gridRegion}>
+                    {regiones.map((region, index) => (
+                      <span
+                        key={index}
+                        className={classNames(
+                          `${getColor(region)}`,
+                          'max-w-min text-ellipsis overflow-hidden text-white rounded-xl whitespace-nowrap  px-3 text-center py-1 font-medium text-sm'
+                        )}
+                      >
+                        {' '}
+                        {region}
+                      </span>
+                    ))}
+                  </div>
+                </td>
+                <td className="pl-10 pr-6 py-4 whitespace-no-wrap border-b  border-gray-200">
+                  {feriado ? <>🎉</> : <>-</>}
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
+  );
 }
+
+import React from 'react';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,161 +1,174 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './styles.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './styles.module.css';
 import classNames from 'classnames';
 
 const departments = [
   {
     id: 1,
-    name: "Boaco",
-    abbreviation: "BO",
-    climate: "☀️ 30°C",
-    municipalities: 6
+    name: 'Boaco',
+    abbreviation: 'BO',
+    climate: '☀️ 30°C',
+    municipalities: 6,
   },
   {
     id: 2,
-    name: "Carazo",
-    abbreviation: "CA",
-    climate: "🌦️ 28°C",
-    municipalities: 8
+    name: 'Carazo',
+    abbreviation: 'CA',
+    climate: '🌦️ 28°C',
+    municipalities: 8,
   },
   {
     id: 3,
-    name: "Chinandega",
-    abbreviation: "CH",
-    climate: "🌤️ 32°C",
-    municipalities: 13
+    name: 'Chinandega',
+    abbreviation: 'CH',
+    climate: '🌤️ 32°C',
+    municipalities: 13,
   },
   {
     id: 4,
-    name: "Chontales",
-    abbreviation: "CT",
-    climate: "🌦️ 29°C",
-    municipalities: 10
+    name: 'Chontales',
+    abbreviation: 'CT',
+    climate: '🌦️ 29°C',
+    municipalities: 10,
   },
   {
     id: 5,
-    name: "Estelí",
-    abbreviation: "ES",
-    climate: "🌤️ 28°C",
-    municipalities: 6
+    name: 'Estelí',
+    abbreviation: 'ES',
+    climate: '🌤️ 28°C',
+    municipalities: 6,
   },
   {
     id: 6,
-    name: "Granada",
-    abbreviation: "GR",
-    climate: "⛅ 31°C",
-    municipalities: 4
+    name: 'Granada',
+    abbreviation: 'GR',
+    climate: '⛅ 31°C',
+    municipalities: 4,
   },
   {
     id: 7,
-    name: "Jinotega",
-    abbreviation: "JI",
-    climate: "🌦️ 27°C",
-    municipalities: 8
+    name: 'Jinotega',
+    abbreviation: 'JI',
+    climate: '🌦️ 27°C',
+    municipalities: 8,
   },
   {
     id: 8,
-    name: "León",
-    abbreviation: "LE",
-    climate: "☀️ 33°C",
-    municipalities: 10
+    name: 'León',
+    abbreviation: 'LE',
+    climate: '☀️ 33°C',
+    municipalities: 10,
   },
   {
     id: 9,
-    name: "Madriz",
-    abbreviation: "MZ",
-    climate: "🌤️ 30°C",
-    municipalities: 9
+    name: 'Madriz',
+    abbreviation: 'MZ',
+    climate: '🌤️ 30°C',
+    municipalities: 9,
   },
   {
     id: 10,
-    name: "Managua",
-    abbreviation: "MA",
-    climate: "🌤️ 31°C",
-    municipalities: 9
+    name: 'Managua',
+    abbreviation: 'MA',
+    climate: '🌤️ 31°C',
+    municipalities: 9,
   },
   {
     id: 11,
-    name: "Masaya",
-    abbreviation: "MY",
-    climate: "🌤️ 32°C",
-    municipalities: 9
+    name: 'Masaya',
+    abbreviation: 'MY',
+    climate: '🌤️ 32°C',
+    municipalities: 9,
   },
   {
     id: 12,
-    name: "Matagalpa",
-    abbreviation: "MT",
-    climate: "🌦️ 29°C",
-    municipalities: 13
+    name: 'Matagalpa',
+    abbreviation: 'MT',
+    climate: '🌦️ 29°C',
+    municipalities: 13,
   },
   {
     id: 13,
-    name: "Nueva Segovia",
-    abbreviation: "NS",
-    climate: "⛅ 30°C",
-    municipalities: 12
+    name: 'Nueva Segovia',
+    abbreviation: 'NS',
+    climate: '⛅ 30°C',
+    municipalities: 12,
   },
   {
     id: 14,
-    name: "RACCN",
-    abbreviation: "RN",
-    climate: "🌧️ 26°C",
-    municipalities: 8
+    name: 'RACCN',
+    abbreviation: 'RN',
+    climate: '🌧️ 26°C',
+    municipalities: 8,
   },
   {
     id: 15,
-    name: "RACCS",
-    abbreviation: "RS",
-    climate: "🌧️ 26°C",
-    municipalities: 12
+    name: 'RACCS',
+    abbreviation: 'RS',
+    climate: '🌧️ 26°C',
+    municipalities: 12,
   },
   {
     id: 16,
-    name: "Río San Juan",
-    abbreviation: "RJ",
-    climate: "🌧️ 26°C",
-    municipalities: 6
+    name: 'Río San Juan',
+    abbreviation: 'RJ',
+    climate: '🌧️ 26°C',
+    municipalities: 6,
   },
   {
     id: 17,
-    name: "Rivas",
-    abbreviation: "RI",
-    climate: "⛅ 32°C",
-    municipalities: 10
-  }
+    name: 'Rivas',
+    abbreviation: 'RI',
+    climate: '⛅ 32°C',
+    municipalities: 10,
+  },
 ];
-
-
 
 export default function Home() {
   return (
     <main className=" flex min-h-screen flex-col items-center xl:px-20  ">
-      
-
-      <section className='pb-10 px-4 w-full  lg:px-10 xl:px-20 xl:pb-20'>
-        <div className='p-4 text-sm md:p-8 md:text-base text-center lg:p-20 text-gray-700 lg:text-lg'>
-          <h2 className='text-md text-center font-bold md:text-xl lg:text-3xl '>Explora nuestras tradiciones culturales y festividades regionales</h2>
-          <p className='hidden lg:block'>Descubre Nicaragua: Departamentos y Festividades</p>
-          <p>Sumérgete en la riqueza cultural y las tradiciones de nuestro país a medida que exploras nuestros días festivos y festividades regionales únicas.</p>
+      <section className="pb-10 px-4 w-full  lg:px-10 xl:px-20 xl:pb-20">
+        <div className="p-4 text-sm md:p-8 md:text-base text-center lg:p-20 text-gray-700 lg:text-lg">
+          <h2 className="text-md text-center font-bold md:text-xl lg:text-3xl ">
+            Explora nuestras tradiciones culturales y festividades regionales
+          </h2>
+          <p className="hidden lg:block">
+            Descubre Nicaragua: Departamentos y Festividades
+          </p>
+          <p>
+            Sumérgete en la riqueza cultural y las tradiciones de nuestro país a
+            medida que exploras nuestros días festivos y festividades regionales
+            únicas.
+          </p>
         </div>
         <ul className={styles.gridView}>
-          {departments.map(({ id, name, climate, municipalities, abbreviation }, index) => (
-            <li key={index} className={classNames(styles.department, styles[abbreviation])}>
-              <div className='flex justify-between'>
-                <span className='underline decoration-2 underline-offset-4'>{id}</span>
-                <span>{abbreviation}</span>
-              </div>
-              <Link className='self-center' href={`/`}>
-                <span className='text-center text-lg md:text-2xl lg:text-3xl'>{name}</span>
-              </Link>
-              <div className='flex justify-between'>
-                <span className="icono-clima">{climate}</span>
-                <span>Municipios: {municipalities}</span>
-              </div>
-            </li>
-          ))}
+          {departments.map(
+            ({ id, name, climate, municipalities, abbreviation }, index) => (
+              <li
+                key={index}
+                className={classNames(styles.department, styles[abbreviation])}
+              >
+                <div className="flex justify-between">
+                  <span className="underline decoration-2 underline-offset-4">
+                    {id}
+                  </span>
+                  <span>{abbreviation}</span>
+                </div>
+                <Link className="self-center" href={`/`}>
+                  <span className="text-center text-lg md:text-2xl lg:text-3xl">
+                    {name}
+                  </span>
+                </Link>
+                <div className="flex justify-between">
+                  <span className="icono-clima">{climate}</span>
+                  <span>Municipios: {municipalities}</span>
+                </div>
+              </li>
+            )
+          )}
         </ul>
       </section>
     </main>
-  )
+  );
 }
